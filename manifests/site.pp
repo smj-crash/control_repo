1825306cb262c94b541db83d74aead29fcd6be51
Steve Jones <smj-crash@users.noreply.github.com>
2020-01-31 23:55:47 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,8 @@
 node 'puppet2.crash.com' {
     file { '/root/README':
-        ensure => file,
+        ensure  => file,
         content => 'This is a README file',
+        owner => 'root',
+        group => 'bin',
     }
 }
