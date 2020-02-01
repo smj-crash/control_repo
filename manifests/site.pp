69f78b0a88cac1a52183fc50f66fb14127aa88cc
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-01 00:04:10 -0800

    "Zero out" the site.pp manifest for now.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,2 @@
 node 'puppet2.crash.com' {
-    file { '/root/README':
-        ensure  => file,
-        content => 'This is a README file',
-        owner => 'root',
-        group => 'bin',
-    }
 }
