6083e6be44a407a7e8e46429b95c084d6b810198
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-07 19:44:40 -0800

    Had file mode defined as an integer - must be a string

--- site/profile/manifests/ssh_root_access.pp
+++ site/profile/manifests/ssh_root_access.pp
@@ -1,7 +1,7 @@
 class profile::ssh_root_access {
   file { '/root/.ssh/authorized_keys':
     ensure => file,
-    mode => 0644,
+    mode => '0644',
     content => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr root@puppet1.crash.com\n"
   }
 }
