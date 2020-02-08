30c423b5c314719fe9deb696097f6fb6215ca6bf
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-07 20:08:00 -0800

    Ensure package is installed and sshd is running

--- site/profile/manifests/ssh_root_access.pp
+++ site/profile/manifests/ssh_root_access.pp
@@ -1,15 +1,22 @@
 class profile::ssh_root_access {
+  package { 'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    enable => 'true',
+    ensure => 'running',
+  }
   file { '/root/.ssh':
     ensure => directory,
     group => 'root',
     mode => '0700',
-    owner => 'root'
+    owner => 'root',
   }
   file { '/root/.ssh/authorized_keys':
     ensure => file,
     group => 'root',
     mode => '0644',
     owner => 'root',
-    content => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr root@puppet1.crash.com\n"
+    content => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr root@puppet1.crash.com\n",
   }
 }
