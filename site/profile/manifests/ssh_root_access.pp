457d4a335438f26d30b83472c84cdb019e2516b1
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-07 20:11:38 -0800

    Try using predefined SSH resource (ssh_authorized_key)

--- site/profile/manifests/ssh_root_access.pp
+++ site/profile/manifests/ssh_root_access.pp
@@ -6,17 +6,23 @@ class profile::ssh_root_access {
     enable => 'true',
     ensure => 'running',
   }
-  file { '/root/.ssh':
-    ensure => directory,
-    group => 'root',
-    mode => '0700',
-    owner => 'root',
+  ssh_authorized_key { 'root@puppet1.crash.com':
+    ensure => present,
+    user => 'root',
+    type => 'ssh-ed25519',
+    key => 'AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr',
   }
-  file { '/root/.ssh/authorized_keys':
-    ensure => file,
-    group => 'root',
-    mode => '0644',
-    owner => 'root',
-    content => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr root@puppet1.crash.com\n",
-  }
+#  file { '/root/.ssh':
+#    ensure => directory,
+#    group => 'root',
+#    mode => '0700',
+#    owner => 'root',
+#  }
+#  file { '/root/.ssh/authorized_keys':
+#    ensure => file,
+#    group => 'root',
+#    mode => '0644',
+#    owner => 'root',
+#    content => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr root@puppet1.crash.com\n",
+#  }
 }
