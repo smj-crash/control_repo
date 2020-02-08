3c18fa10a2f16f179fbd3c3014d0725bdbb80a4c
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-07 22:15:00 -0800

    Adding node "dockermc" for Minecraft exercise

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,17 +1,29 @@
 class profile::agent_nodes {
+
   include dockeragent
+  
   dockeragent::node {
     'dockerdb.crash.com':
-  }
-  dockeragent::node {
-    'dockerweb.crash.com':
   }
   host { 'dockerdb.crash.com':
     ensure => present,
     ip     => '172.18.0.2',
   }
+  
+  dockeragent::node {
+    'dockermc.crash.com':
+  }
+  host { 'dockermc.crash.com':
+    ensure => present,
+    ip     => '172.18.0.4',
+  }
+  
+  dockeragent::node {
+    'dockerweb.crash.com':
+  }
   host { 'dockerweb.crash.com':
     ensure => present,
     ip     => '172.18.0.3',
   }
+  
 }
