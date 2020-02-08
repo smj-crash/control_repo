e5738a6aff61d47168d556914ac127b0eb662721
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-07 20:02:16 -0800

    Make ownership of authorized_keys explicit

--- site/profile/manifests/ssh_root_access.pp
+++ site/profile/manifests/ssh_root_access.pp
@@ -7,7 +7,9 @@ class profile::ssh_root_access {
   }
   file { '/root/.ssh/authorized_keys':
     ensure => file,
+    group => 'root',
     mode => '0644',
+    owner => 'root',
     content => "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERLOAXQSEGhgX5T0mgSR6IbymfSIXVODFbpKNyfpVlr root@puppet1.crash.com\n"
   }
 }
