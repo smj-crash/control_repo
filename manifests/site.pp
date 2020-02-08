aee7190b5fb236ada96575cd347c303c1370eaa0
Steve Jones <smj-crash@users.noreply.github.com>
2020-02-07 16:03:25 -0800

    Regex rules for Docker container roles

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,15 @@
 node default {
 }
 node 'puppet1.crash.com' {
- include role::master_server 
+  include role::master_server 
 }
 node 'puppet2.crash.com' {
- include role::docker_host
+  include role::docker_host
+}
+
+node /^dockerdb/ {
+  include role::db_server
+}
+node /^dockerweb/ {
+  include role::app_server
 }
