63bad12cd79baceacb3e1dfc57d33c8e0325c3f8
Steven M Jones <smj+github@crash.com>
2020-05-19 00:17:59 -0700

    Trying to instantiate thias-postfix

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,6 +2,7 @@ class profile::base {
 
   include profile::base_packages
   include profile::ssh_root_access
+  include profile::localmta
 
   user {'admin':
     ensure => present,
---
+++ site/profile/manifests/localmta.pp
@@ -0,0 +1,19 @@
+class profile::localmta {
+
+  package { 'postfix':
+    ensure => present,
+  }
+
+  service { 'postfix':
+    enable => 'true',
+    ensure => 'running',
+  }
+
+  class { 'postfix::server' :
+    myhostname => ${fqdn},
+    mydomain => ${domain},
+    inet_interfaces => 'localhost',
+    mydestination => 'segv.crash.com',
+    message_size_limit => '10485760',
+    submission => true,
+  }
